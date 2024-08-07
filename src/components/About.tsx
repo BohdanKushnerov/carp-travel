790d79d15b332e34f1cab4a88cad78018025bf16
Bohdan Kushnerov <bohdan.kushnerov@gmail.com>
2024-08-07 22:14:03 +0300

    add desktop styles for about section

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -4,14 +4,14 @@ import Section from './Section';
 const About = () => {
   return (
     <Section className="py-[56px]">
-      <div className="md:grid-custom-rows mx-auto w-full max-w-[440px] md:grid md:max-w-[704px] md:grid-cols-2 md:grid-rows-3">
+      <div className="md:grid-custom-three-rows xl:grid-custom-two-rows md:grid-cols-auto mx-auto w-full max-w-[440px] md:grid md:max-w-[704px] md:grid-cols-2 md:grid-rows-3 xl:max-w-[1120px] xl:grid-rows-2">
         {/*  */}
-        <h2 className="order-1 mb-[8px] text-[40px] font-thin uppercase leading-[140%] tracking-[-0.04em] md:w-[412px] md:text-[67px] md:leading-[100%]">
+        <h2 className="order-1 mb-[8px] text-[40px] font-thin uppercase leading-[140%] tracking-[-0.04em] md:mb-0 md:w-[412px] md:text-[67px] md:leading-[100%] xl:w-[606px] xl:text-[98px]">
           WHO <b className="font-medium">WE ARE</b>
         </h2>
-        {/* */}
-        <div className="order-2 mb-[40px] w-[180px] text-[14px] font-extralight leading-[143%] md:row-span-2 md:mb-[64px] md:ml-auto md:w-[220px] md:text-[16px] md:leading-[125%]">
-          <p className="mb-[20px]">
+        {/*  */}
+        <div className="order-2 mb-[40px] w-[180px] text-[14px] leading-[143%] md:row-span-2 md:mb-[64px] md:ml-auto md:w-[220px] md:text-[16px] md:leading-[125%] xl:row-span-1 xl:mb-[72px] xl:ml-[24px] xl:w-[292px] xl:text-[18px] xl:leading-[133%]">
+          <p className="mb-[20px] md:mb-[16px] xl:mb-[24px]">
             <strong className="font-normal"> a team of enthusiasts </strong>
             who are fully committed to the mission of creating unforgettable and
             extraordinary trips to the most beautiful parts of the Carpathians.
@@ -27,8 +27,26 @@ const About = () => {
             creating unforgettable memories.
           </p>
         </div>
-        {/* */}
-        <h3 className="order-3 mb-[40px] ml-auto w-[180px] text-[14px] font-extralight leading-[143%] md:mb-[64px] md:ml-0 md:mt-auto md:w-[221px] md:text-[16px] md:leading-[125%]">
+        {/*font-weight: 400;
+font-size: 18px;
+line-height: 133%;
+text-transform: uppercase;
+color: #fff;
+
+font-weight: 400;
+font-size: 18px;
+line-height: 133%;
+text-transform: uppercase;
+text-align: right;
+color: #fff;
+
+font-family: var(--font-family);
+font-weight: 200;
+font-size: 18px;
+line-height: 133%;
+letter-spacing: 0.12em;
+color: #fff;*/}
+        <h3 className="order-3 mb-[40px] ml-auto w-[180px] text-[14px] leading-[143%] md:mb-[64px] md:ml-0 md:mt-auto md:w-[221px] md:text-[16px] md:leading-[125%] xl:order-4 xl:mb-0 xl:ml-auto xl:w-[297px] xl:text-[18px] xl:leading-[133%]">
           <b className="font-normal">
             <span className="block uppercase">From vacationers</span>
             <span className="block text-right uppercase">
@@ -39,8 +57,8 @@ const About = () => {
             we have a tour for everyone.
           </span>
         </h3>
-        {/*  */}
-        <p className="order-4 text-[14px] font-extralight leading-[143%] md:col-span-2 md:ml-auto md:max-w-[463px] md:text-[16px] md:leading-[125%]">
+        {/* */}
+        <p className="order-4 text-[14px] leading-[143%] md:col-span-2 md:ml-auto md:max-w-[463px] md:text-[16px] md:leading-[125%] xl:order-3 xl:col-span-1 xl:ml-0 xl:mr-auto xl:w-[605px] xl:text-[18px] xl:leading-[133%]">
           <strong className="font-normal">We use methods </strong> that are
           time-tested and proven. Our expert guides with in-depth knowledge of
           the Carpathian landscapes lead you safely through the mysteries of
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -5,7 +5,7 @@ import JoinNowBtn from './JoinNowBtn';
 const Hero: FC = () => {
   return (
     <Section className="bg-hero-mobile bg-cover bg-center bg-no-repeat pb-[56px] pt-[105px] md:pt-[121px] xl:pt-[130px]">
-      <div className="md:grid-custom-rows mx-auto w-full max-w-[440px] md:grid md:max-w-[704px] md:grid-cols-2 md:grid-rows-3 md:items-baseline xl:max-w-[1120px]">
+      <div className="md:grid-custom-three-rows mx-auto w-full max-w-[440px] md:grid md:max-w-[704px] md:grid-cols-2 md:grid-rows-3 md:items-baseline xl:max-w-[1120px]">
         {/*  */}
         <div className="mb-[24px] ml-auto flex w-[118px] flex-col md:order-2 md:mb-[56px] md:w-[230px] xl:mb-[181px] xl:w-[305px]">
           <h2 className="text-[37px] font-thin leading-[37px] tracking-[0.04em] md:text-[67px] md:leading-[55px] md:tracking-[0.08em] xl:text-[98px] xl:leading-[98px] xl:tracking-[0em]">
